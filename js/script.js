80ada1dbd9f223127fdb411f1024934d872b035f
tecsxpert <dev@tecsxpert.xyz>
2025-12-27 15:20:00 +0530

    Bug Fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL="https://todo.api.eventslab.in/api";
-const API_BASE_URL="http://localhost:8082/api";
+const API_BASE_URL="https://todo.api.eventslab.in/api";
+//const API_BASE_URL="http://localhost:8082/api";
 let currentEditingTask = null;
 
 // Initialize application
